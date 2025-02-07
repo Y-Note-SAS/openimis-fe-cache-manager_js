471431d232525392b69e39c60259efe1fa0eb1d7
vaneck237 <vaneckbilongo@gmail.com>
2025-02-07 11:17:45 +0100

    print cache values

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import CacheMainMenu from "./menus/CacheMainMenu";
 import messages_en from "./translations/en.json";
+import messages_fr from "./translations/fr.json";
 import CachingDashboardPage from "./pages/CachingDashboardPage";
 
-const CACHING_DASHBOARD = "cache_dashboard/dashboard"
+const CACHING_DASHBOARD = "cache/dashboard"
 
 const DEFAULT_CONFIG = {
-  "translations": [{ key: "en", messages: messages_en }],
-  "core.MainMenu": [CacheMainMenu],
+  "translations": [{ key: "en", messages: messages_en }, {key: "fr", messages: messages_fr}],
   "core.Router": [
    { path: CACHING_DASHBOARD, component: CachingDashboardPage},
   ]
--- src/pages/CachingDashboardPage.js
+++ src/pages/CachingDashboardPage.js
@@ -1,21 +1,111 @@
 
-import React, { Component, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React, { Component, Fragment, useState } from "react";
 import { withTheme, withStyles } from "@material-ui/core/styles";
+import { injectIntl } from "react-intl";
+import { Searcher } from "@openimis/fe-core";
+import DeleteIcon from "@material-ui/icons/Delete";
+import { IconButton, Grid } from "@material-ui/core"
+import {
+    formatMessageWithValues,
+    Table,
+    formatMessage,
+    withModulesManager,
+    TextInput
+} from "@openimis/fe-core";
 
 const styles = theme => ({
     page: theme.page,
 });
 
 class CachingDashboardPage extends Component {
+    state = {
+        elements: [],
+    };
+
+    _onDelete = (key) => {
+    };
+
+    _getdata = () => {
+
+        // for (const key of keys) {
+        //     //data.push(key);
+        //     // const isOurCache = key.startsWith("myapp-");
+        //     // if (currentCache === key || !isOurCache) {
+        //     //     continue;
+        //     // }
+        //     // caches.delete(key);
+        // }
+    }
+
+    initData = () => {
+        this._getdata();
+        let data = [];
+        var localKeys = Object.keys(localStorage);
+        var sessionKeys = Object.keys(sessionStorage);
+        var i = localKeys.length;
+        var j = sessionKeys.length;
+        console.log(localStorage)
+        console.log(sessionStorage)
+
+        while (i--) {
+            data.push(localKeys[i]);
+        }
+        while (j--) {
+            data.push(sessionKeys[j]);
+        }
+
+        if ('caches' in window) {
+            caches.open('localhost').then(function(cache) {
+                console.log(cache);
+              }).catch(function(error) {
+                // Failed to open cache
+              });
+        }
+        return data;
+    };
+
+    componentDidMount() {
+        this.setState({ elements: this.initData() });
+    }
+
     render() {
-        const {classes } = this.props 
+        const { intl, classes } = this.props
+
+        var count = this.state.elements.length;
+
+        let headers = [
+            "cacheSummaries.key",
+            "cacheSummaries.description"
+        ]
+
+        let itemFormatters = [
+            (i) => (
+                <Grid item xs={4} className={classes.item}>
+                    {i}
+                </Grid>
+            ),
+            (i) => <Grid item xs={8} className={classes.item}>{localStorage.getItem(i)}</Grid>,
+            (i) => (
+                <IconButton onClick={(e) => this._onDelete(i)}>
+                    <DeleteIcon />
+                </IconButton>
+            )
+        ];
+
         return (
             <div className={classes.page}>
-                Test 
+                <Table
+                    module="cache"
+                    header={formatMessageWithValues(intl, "cache", "cacheSummaries", { count })}
+                    headers={headers}
+                    itemFormatters={itemFormatters}
+                    items={this.state.elements}
+                />
             </div>
+
+
         )
     }
 }
 
-export default withTheme(withStyles(styles)(CachingDashboardPage)) ;
+export default withModulesManager(injectIntl(withTheme(withStyles(styles)(CachingDashboardPage))));
